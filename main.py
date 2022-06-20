3dc7f342fc43a7d713cd439ea3e856cf1a7f2fc6
Patrick <me@patrickphat.com>
2022-06-20 18:07:10 +0700

    Update first draft

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request":request})
+
+
