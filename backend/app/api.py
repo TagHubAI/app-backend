7e7ea3877f76792a7a01dd8b2535ff548bc9c2b6
Patrick <me@patrickphat.com>
2022-06-22 11:27:31 +0700

    Update async graph loading using Alpine

--- backend/app/api.py
+++ backend/app/api.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import asyncio
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -14,4 +15,9 @@ templates = Jinja2Templates(directory="templates")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request":request})
 
+@app.get("/slow_call/{time_lapse}")
+async def read_root(request: Request, time_lapse: int):
+    import time
+    await asyncio.sleep(time_lapse)
+    return f"Return after {time_lapse} sec!"
 
