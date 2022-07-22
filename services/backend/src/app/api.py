e4405a7c5c42b733fde5ce4007fdf5287b166192
Patrick <me@patrickphat.com>
2022-07-22 22:39:43 +0700

    Update

--- services/backend/src/app/api.py
+++ services/backend/src/app/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Depends, Form
+from fastapi import FastAPI, Request, Depends, Form, File, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -8,7 +8,24 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import asyncio
 
 from ..db.config import TORTOISE_ORM_CONFIG
-from ..db.models import Users, User_Pydantic, UserIn_Pydantic, UserOut_Pydantic
+from ..db.models import (
+    Users, 
+    User_Pydantic, 
+    UserIn_Pydantic, 
+    UserOut_Pydantic, 
+    Workflows,
+    Workflow_Pydantic, 
+    WorkflowIn_Pydantic, 
+    WorkflowOut_Pydantic,
+    Datasets,
+    Dataset_Pydantic,
+    DatasetIn_Pydantic,
+    DatasetOut_Pydantic,
+    Datapoints,
+    Datapoint_Pydantic,
+    DatapointIn_Pydantic,
+    DatapointOut_Pydantic,
+    )
 
 from tortoise.contrib.fastapi import register_tortoise
 import starlette.status as status
@@ -22,33 +39,69 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 register_tortoise(app, **TORTOISE_ORM_CONFIG)
 
-@app.get("/")
-def get_root():
-    return "Hello World! TagHub is awesome!"
+@app.post("/api/v1/workflows", response_model=List[WorkflowOut_Pydantic])
+async def list_workflows():
+    return await WorkflowOut_Pydantic.from_queryset(Workflows.all())
 
-@app.get("/api/v1/users", response_model = List[UserOut_Pydantic])
-async def get_users():
-    return await UserOut_Pydantic.from_queryset(Users.all())
+@app.post("/api/v1/workflows", response_model = WorkflowOut_Pydantic)
+async def create_workflow(workflow: WorkflowIn_Pydantic):
+    workflow_obj = await Workflows.create(**workflow.dict(exclude_unset=True))
+    return await WorkflowOut_Pydantic.from_tortoise_orm(workflow_obj)
 
-@app.get("/api/v1/users/{user_id}", response_model = UserOut_Pydantic)
-async def get_user(user_id: int):
-    return await UserOut_Pydantic.from_queryset_single(Users.get(id=user_id))
+@app.post("/api/v1/workflows/{workflow_id}/dataset/upload")
+async def upload_dataset(file: UploadFile):
+    ACCEPTED_EXT = [".csv"]
 
-@app.post("/api/v1/user", response_model = UserOut_Pydantic)
-async def create_user(user: UserIn_Pydantic):
-    user_obj = await Users.create(**user.dict(exclude_unset=True))
-    return await User_Pydantic.from_tortoise_orm(user_obj)
+    filename = file.filename
+    if not any([filename.endswith(ext) for ext in ACCEPTED_EXT]):
+        return HTTPException(status_code="415", detail=f"Expected extension to be in {ACCEPTED_EXT} but got '{file.filename}'")
+    
+    workflow = Workflows.filter(id=workflow_id)
 
-@app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = User_Pydantic.from_queryset_single(Users.get(username=form_data.get('username'))) 
+    if filename.endswith(".csv"):
+        import csv
+        with open(filename, 'r') as file:
+            my_reader = csv.reader(file, delimiter=',')
+            for row in my_reader:
+                print(row)
+                o
+    
+    return {"filename": file.filename}
 
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+@app.get("/api/v1/workflows/{idx}/dataset")
+async def list_dataset():
+    pass
 
-    return {"access_token": user.username, "token_type": "bearer"}
+
+#### POSTPONED UNTIL HAVE USERS MANAGEMENT
+# @app.get("/")
+# def get_root():
+#     return "Hello World! TagHub is awesome!"
+
+# @app.get("/api/v1/users", response_model = List[UserOut_Pydantic])
+# async def get_users():
+#     return await UserOut_Pydantic.from_queryset(Users.all())
+
+# @app.get("/api/v1/users/{user_id}", response_model = UserOut_Pydantic)
+# async def get_user(user_id: int):
+#     return await UserOut_Pydantic.from_queryset_single(Users.get(id=user_id))
+
+# @app.post("/api/v1/user", response_model = UserOut_Pydantic)
+# async def create_user(user: UserIn_Pydantic):
+#     user_obj = await Users.create(**user.dict(exclude_unset=True))
+#     return await UserOut_Pydantic.from_tortoise_orm(user_obj)
+
+# @app.post("/api/v1/authorize")
+# async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+#     user_dict = User_Pydantic.from_queryset_single(Users.get(username=form_data.get('username'))) 
+
+#     if not user_dict:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+#     user = UserInDB(**user_dict)
+#     hashed_password = fake_hash_password(form_data.password)
+#     if not hashed_password == user.hashed_password:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+#     return {"access_token": user.username, "token_type": "bearer"}
+
 
--- services/backend/src/db/models.py
+++ services/backend/src/db/models.py
@@ -9,27 +9,37 @@ class Users(models.Model):
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
 
-User_Pydantic = pydantic_model_creator(Users, name="User")
-UserIn_Pydantic = pydantic_model_creator(Users, name="UserIn", exclude_readonly=True)
-UserOut_Pydantic = pydantic_model_creator(Users, name="UserOut", exclude=("password",))
+class Workflows(models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
 
 class Datasets(models.Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=255)
-    owner = fields.ForeignKeyField("models.Users", related_name="dataset")
+    workflow = fields.ForeignKeyField("models.Workflows", related_name="dataset")
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
-
-Dataset_Pydantic = pydantic_model_creator(Datasets, name="Dataset")
-DatasetIn_Pydantic = pydantic_model_creator(Datasets, name="DatasetIn", exclude_readonly=True)
-DatasetOut_Pydantic = pydantic_model_creator(Datasets, name="DatasetOut", exclude_readonly=True)
 
 class Datapoints(models.Model):
     id = fields.IntField(pk=True)
     content = fields.CharField(max_length=255)
-    dataset = fields.ForeignKeyField("models.Users", related_name="datapoint")
+    dataset = fields.ForeignKeyField("models.Datasets", related_name="datapoint")
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
+
+User_Pydantic = pydantic_model_creator(Users, name="User")
+UserIn_Pydantic = pydantic_model_creator(Users, name="UserIn", exclude_readonly=True)
+UserOut_Pydantic = pydantic_model_creator(Users, name="UserOut", exclude=("password",))
+
+Workflow_Pydantic = pydantic_model_creator(Workflows, name="Workflow")
+WorkflowIn_Pydantic = pydantic_model_creator(Workflows, name="WorkflowIn", exclude_readonly=True)
+WorkflowOut_Pydantic = pydantic_model_creator(Workflows, name="WorkflowOut")
+
+Dataset_Pydantic = pydantic_model_creator(Datasets, name="Dataset")
+DatasetIn_Pydantic = pydantic_model_creator(Datasets, name="DatasetIn", exclude_readonly=True)
+DatasetOut_Pydantic = pydantic_model_creator(Datasets, name="DatasetOut", exclude_readonly=True)
 
 Datapoint_Pydantic = pydantic_model_creator(Datapoints, name="Datapoint")
 DatapointIn_Pydantic = pydantic_model_creator(Datapoints, name="DatapointIn", exclude_readonly=True)
